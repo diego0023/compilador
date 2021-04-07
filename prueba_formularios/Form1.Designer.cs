aca97be92a993c1f91d1d6d0f9c0ce47b1e53c89
diego0023 <diegogalan.0023@gmail.com>
2021-04-07 10:53:50 -0600

    Agregar archivos de proyecto.

---
+++ prueba_formularios/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace prueba_formularios
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtContenido = new System.Windows.Forms.TextBox();
+            this.btnSeleccionar = new System.Windows.Forms.Button();
+            this.Archivo = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(27, 27);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(220, 23);
+            this.txtDireccion.TabIndex = 0;
+            // 
+            // txtContenido
+            // 
+            this.txtContenido.Location = new System.Drawing.Point(27, 77);
+            this.txtContenido.Multiline = true;
+            this.txtContenido.Name = "txtContenido";
+            this.txtContenido.Size = new System.Drawing.Size(300, 298);
+            this.txtContenido.TabIndex = 1;
+            // 
+            // btnSeleccionar
+            // 
+            this.btnSeleccionar.Location = new System.Drawing.Point(266, 26);
+            this.btnSeleccionar.Name = "btnSeleccionar";
+            this.btnSeleccionar.Size = new System.Drawing.Size(75, 23);
+            this.btnSeleccionar.TabIndex = 2;
+            this.btnSeleccionar.Text = "buscar";
+            this.btnSeleccionar.UseVisualStyleBackColor = true;
+            this.btnSeleccionar.Click += new System.EventHandler(this.btnSeleccionar_Click);
+            // 
+            // Archivo
+            // 
+            this.Archivo.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 464);
+            this.Controls.Add(this.btnSeleccionar);
+            this.Controls.Add(this.txtContenido);
+            this.Controls.Add(this.txtDireccion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtContenido;
+        private System.Windows.Forms.Button btnSeleccionar;
+        private System.Windows.Forms.OpenFileDialog Archivo;
+    }
+}
+
---
+++ prueba_formularios/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba_formularios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void btnSeleccionar_Click(object sender, EventArgs e)
+        {
+            Archivo.InitialDirectory = "C:/";
+            if (Archivo.ShowDialog()== DialogResult.OK)
+            {
+                this.txtDireccion.Text = Archivo.FileName;
+            }
+
+            System.IO.StreamReader sr = new System.IO.StreamReader(txtDireccion.Text,System.Text.Encoding.Default);
+
+            string contenido;
+            
+            contenido = sr.ReadToEnd();
+            sr.Close();
+            txtContenido.Text = contenido;
+
+        }
+    }
+}
